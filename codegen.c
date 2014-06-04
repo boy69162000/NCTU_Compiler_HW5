a0b0f7f0099433512b212e297886d00bbf0f417e
boy69162000 <boy69162000@gmail.com>
2014-06-04 16:04:02 +0800

    make a little try on code gen

---
+++ codegen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include "header.h"
+#include "symbolTable.h"
+
+void codeGen(AST_NODE* prog);
+
+void codeGen(AST_NODE* prog) {
+
+    FILE *output = fopen("w", "output.s");
+    
+
+    return;
+}
